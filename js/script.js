936f251264d44d31f66793a8ff8a7e614e6ded9c
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-30 02:51:26 +0000

    Finished script.js comments

--- js/script.js
+++ js/script.js
@@ -23,40 +23,60 @@ function displayTriangle() {
     results = "Please enter a positive integer greater than 0.";
     }
      do {
+       
+       // if the counter is a multiple of 5, repeat planet symbols
        if (counter % 5 == 0) {
+
+        // repeat the planet symbol the number of times that the counter is equal to
          star= "o".repeat(counter)
+
+         // add a line break
           results= results+star+"<br>"
           counter++
        }
+
+        //otherwise, if the counter is not a multiple of 5, repeat the star symbol
        else if (counter % 5 != 0) {
+         
+         // repeat the star symbol the number of times that the counter is equal to.
           star= "*".repeat(counter)
+
+          // add a line break
           results= results+star+"<br>"
+
+         //increment the counter
           counter++
         }
      } while (counter <= height)
 
   // initialize resultsUpsideDown to an empty string
-
-  // initialize counterUpsideDown to 0
   let resultsUpsideDown= ""
+  // initialize counterUpsideDown to 0
   let starUpsideDown = ""
-  // initialize factorial to 1
-  
+
      for (let counterUpsideDown=height;counterUpsideDown >= 0; counterUpsideDown--) {
-      
+
+       // if the counter is a multiple of 5, repeat planet symbols
        if (counterUpsideDown % 5 == 0) {
+        // repeat the planet symbol the number of times that the counter is equal to.
         starUpsideDown= "o".repeat(counterUpsideDown)
+
+        //add a line break
         resultsUpsideDown= resultsUpsideDown+starUpsideDown+"<br>"
        }
+         
+        //otherwise, if the counter is not a multiple of 5, repeat the star symbol
         else if (counterUpsideDown % 5 != 0) {
+           // repeat the star symbol the number of times that the counter is equal to.
           starUpsideDown = "* ".repeat(counterUpsideDown)
+
+       // add a line break
       resultsUpsideDown= resultsUpsideDown+starUpsideDown+"<br>"
       }
      }
 
-  
+  // display results
   document.getElementById('program-feedback').innerHTML = results+resultsUpsideDown
-    // define the resultsUpsideDown statement
   
 }
 
